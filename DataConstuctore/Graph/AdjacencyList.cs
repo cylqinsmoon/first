d5c8299999986b9943210a6653e6fe94e2612194
cylqinsmoon <41931803+cylqinsmoon@users.noreply.github.com>
2019-02-23 14:24:39 +0800

    Add files via upload

---
+++ DataConstuctore/Graph/AdjacencyList.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    class AdjacencyList<T>
+    {
+        //嵌套类,表示存放于数组中的表头结点
+        public class Vertex<TValue>
+        {
+            public TValue data;//数据
+            public Node firstEdge;//邻接点链表头指针
+            public Boolean visited;//访问标志，遍历时使用
+            public Vertex(TValue value)//构造方法
+            {
+                data = value;
+            }
+        }
+        //嵌套类,表示链表中的表结点
+        public class Node
+        {
+            public Vertex<T> adjvex;//邻接点域
+            public Node next;//下一个邻接点指针域
+            public Node(Vertex<T> value)
+            {
+                adjvex = value;
+            }
+        }
+        //成员
+        List<Vertex<T>> items;//图的顶点集合
+        //方法
+        public AdjacencyList() : this(10) { }//构造方法
+        public  AdjacencyList(int capacity)//指定容量的构造方法
+        {
+            items = new List<Vertex<T>>(capacity);
+        }
+        //查找图中是否包含某项
+        public bool Contains(T item)
+        {
+            foreach(Vertex<T> v in items)
+            {
+                if(v.data.Equals(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        //查找指定项并返回
+        private Vertex<T> Find(T item)
+        {
+            foreach(Vertex<T> v in items)
+            {
+                if(v.data.Equals(item))
+                {
+                    return v;
+                }
+            }
+            return null;
+        }
+        //添加一个顶点
+        public void AddVertex(T item)
+        {
+            //不允许插入重复值
+            if(Contains(item))
+            {
+                throw new ArgumentException("插入了重复的顶点！");
+            }
+            items.Add(new Vertex<T>(item));
+        }
+        //添加有向边
+        private void AddDiretedEdge(Vertex<T> fromVer,Vertex<T> toVer)
+        {
+            if(fromVer.firstEdge==null)//无邻接点时
+            {
+                fromVer.firstEdge = new Node(toVer);
+            }
+            else
+            {
+                Node tmp, node = fromVer.firstEdge;
+                do
+                {
+                    //检查是否添加了重复边
+                    if (node.adjvex.data.Equals(toVer.data))
+                    {
+                        throw new ArgumentException("添加了重复的边！");
+                    }
+                    tmp = node;
+                    node = node.next;
+                } while (node != null);
+                tmp.next = new Node(toVer);//添加到链表末尾
+            }
+        }
+        //添加无向边
+        public void AddEdge(T from,T to)
+        {
+            Vertex<T> fromVer = Find(from);//找到起始顶点
+            if(fromVer==null)
+            {
+                throw new ArgumentException("头顶点并不存在！");
+            }
+            Vertex<T> toVer = Find(to);//找到结束顶点
+            if(toVer==null)
+            {
+                throw new ArgumentException("尾顶点并不存在！");
+            }
+            //无向边的两个顶点都需记录边信息
+            AddDiretedEdge(fromVer, toVer);
+            AddDiretedEdge(toVer,fromVer);
+        }
+        //测试打印
+        public override string ToString()
+        {
+            //打印每个顶点和它的邻接点
+            string s = string.Empty;
+            foreach(Vertex<T> v in items)
+            {
+                s += v.data.ToString() + ":";
+                if(v.firstEdge!=null)
+                {
+                    Node tmp = v.firstEdge;
+                    while(tmp!=null)
+                    {
+                        s += tmp.adjvex.data.ToString();
+                        tmp = tmp.next;
+                    }
+                }
+                s += "\r\n";
+            }
+            return s;
+        }
+        //初始化visited访问标志
+        private void InitVisited()
+        {
+            foreach(Vertex<T> v in items)
+            {
+                v.visited = false;
+            }
+        }
+        //使用递归进行深度优先遍历
+        private void DFS(Vertex<T> v)
+        {
+            v.visited = true;
+            Console.Write(v.data + " ");
+            Node node = v.firstEdge;//访问顺序与添加边顺序有关，与添加的点顺序有关
+            while(node!=null)
+            {
+                //如果邻接点未被访问，则递归访问它的边
+                if(!node.adjvex.visited)
+                {
+                    DFS(node.adjvex);//递归
+                }
+                node = node.next;
+            }
+        }
+        public void DFSTraverse()
+        {
+            InitVisited();//将visited标志全部设置为false
+            //若图为非连通图，需访问不同的顶点
+            foreach(Vertex<T> v in items)
+            {
+                if(!v.visited)//当visited为false表示未被访问
+                {
+                    DFS(v);
+                }
+            }
+        }
+        //使用队列进行广度优先遍历
+        public void BFS(Vertex<T> v)
+        {
+            //创建一个队列
+            Queue<Vertex<T>> queue = new Queue<Vertex<T>>();
+            Console.Write(v.data + " ");
+            v.visited = true;
+            queue.Enqueue(v);
+            while(queue.Count>0)//只要队列不为空就循环
+            {
+                Vertex<T> w = queue.Dequeue();
+                Node node = w.firstEdge;
+                while(node!=null)
+                {
+                    //如果邻接点未被访问，则递归访问它的边
+                    if(!node.adjvex.visited)
+                    {
+                        Console.Write(node.adjvex.data + " ");
+                        node.adjvex.visited = true;
+                        queue.Enqueue(node.adjvex);
+                    }
+                    node = node.next;//访问下一个邻接点
+                }
+            }
+        }
+        //广度优先遍历
+        public void BFSTraverse()
+        {
+            InitVisited();
+            foreach(Vertex<T> v in items)
+            {
+                if(!v.visited)
+                {
+                    BFS(v);
+                }
+            }
+        }
+    }
+}
---
+++ DataConstuctore/Graph/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Graph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdjacencyList<int> graph1 = new AdjacencyList<int>();
+            //添加顶点
+            graph1.AddVertex(1);
+            graph1.AddVertex(2);
+            graph1.AddVertex(3);
+            graph1.AddVertex(4);
+            graph1.AddVertex(5);
+            graph1.AddVertex(6);
+            graph1.AddVertex(10);
+            graph1.AddVertex(7);
+            graph1.AddVertex(8);
+            graph1.AddVertex(9);          
+            //添加边
+            graph1.AddEdge(1, 2);  
+            graph1.AddEdge(1, 4);
+            graph1.AddEdge(1, 3);
+            graph1.AddEdge(4, 6);           
+            graph1.AddEdge(3, 5);
+            graph1.AddEdge(10, 8);
+            graph1.AddEdge(10,7);
+            graph1.AddEdge(8,9);           
+            Console.WriteLine("graph1结构为：");
+            Console.WriteLine(graph1.ToString());
+            Console.WriteLine("深度优先遍历graph1:");
+            graph1.DFSTraverse();
+            Console.WriteLine();
+            Console.WriteLine("广度优先遍历graph1：");
+            graph1.BFSTraverse();
+            int[,] LJmini = new int[6, 6];
+            LJmini[0, 1] = 1;
+            LJmini[0, 3] = 3;
+            LJmini[3, 4] = 2;
+            LJmini[0, 4] = 5;
+            LJmini[0, 5] = 5;
+            LJmini[1, 2] = 3;
+            LJmini[1, 5] = 2;                      
+            Console.WriteLine();
+            Dijkstra(LJmini, 0);
+            Floyd(LJmini);
+            Console.ReadKey();
+        }
+        static void Dijkstra(int[,] cost,int v)//cost为邻接矩阵，v为源点
+        {
+            int n = cost.GetLength(1);//顶点个数.即Array指定维度数
+            int[] s = new int[n];//集合S
+            int[] dist = new int[n];//结果集
+            int[] path = new int[n];//存放路径
+            for (int i = 0; i < n; i++)
+            {
+                //结果集初始化，将邻接矩阵源点所表示的行数据加进dist集合
+                dist[i] = cost[v, i];
+                if(cost[v,i]>0)//路径初始化
+                {
+                    //如果某顶点与源点存在边
+                    path[i] = v;
+                }
+                else//如果某顶点与源点不存在边
+                {
+                    //它的前一顶点值设为-1
+                    path[i] = -1; 
+                }
+            }
+            s[v] = 1;//将抽取出的顶点加进集合S
+            path[v] = 0;
+            for(int i=0; i<n;i++)
+            {
+                int u = 0, mindis = int.MaxValue;
+                //u表示剩余顶点在dist集合中的最小值所在索引
+                for(int j=0;j<n;j++)//寻找dist集合中的最小值
+                {
+                    if(s[j]==0&&dist[j]>0&&dist[j]<mindis)
+                    {
+                        u = j;
+                        mindis = dist[j];
+                    }
+                }
+                s[u] = 1;//将抽取出来的顶点放入集合S中
+                for(int j=0;j<n;j++)
+                {
+                    if(s[j]==0)//如果顶点不在集合S中
+                    {
+                        //加入的顶点与其顶点存在边，并且新计算的值小于原值
+                        if (cost[u, j] > 0 && (dist[j] == 0 ||
+                            dist[u] + cost[u, j] < dist[j]))
+                        {
+                            //用更小的值代替原值
+                            dist[j] = dist[u] + cost[u, j];
+                            path[j] = u;//记录加入点路径上的前一顶点
+                        }
+                    }
+                }
+            }
+            //打印源点到各顶点的路径及距离
+            for (int i = 0; i < n; i++)
+            {
+                if(s[i]==1)
+                {
+                    Console.Write("从{0}到{1}的最短路径为：", v, i);
+                    Console.Write(v + "→");
+                    GetPath(path, i, v);
+                    Console.Write(i);
+                    Console.WriteLine("    路径长度为："+dist[i]);
+                }
+            }
+        }
+        static void GetPath(int[] path, int i, int v)
+        {
+            int k = path[i];
+            if (k == v)
+            {
+                return;
+            }
+            GetPath(path, k, v);
+            Console.Write(k + "→");
+        }
+        static void Floyd(int[,] cost)
+        {
+            int n = cost.GetLength(1);//图中顶点个数
+            int[,] A = new int[n, n];//存放最短路径长度
+            int[,] path = new int[n, n];//存放最短路径信息
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0; j<n;j++)
+                {
+                    //辅助数组A和path的初始化
+                    A[i, j] = cost[i, j];
+                    path[i, j] = -1;
+                }
+            }
+            //弗洛伊德算法核心代码
+            for(int k=0;k<n;k++)
+            {
+                for(int i=0;i<n;i++)
+                {
+                    for(int j=0;j<n;j++)
+                    {//如果存在通过中间点k的路径
+                        if (i!=j&&A[i,k]!=0&&A[k,j]!=0)
+                        {//如果加入中间点k的路径更短
+                            if (A[i, j] == 0 || A[i,j]>A[i,k]+A[k,j])
+                            {//用新路径代替原路径
+                                A[i, j] = A[i, k] + A[k, j];
+                                path[i, j] = k;
+                            }
+                        }
+                    }
+                }
+                //打印最短路径及路径长度
+                for(int i=0;i < n;i++)
+                {
+                    for(int j=0; j<n;j++)
+                    {
+                        if(A[i,j]==0)
+                        {
+                            if(i!=j)
+                            {
+                                Console.WriteLine("从{0}到{1}没有路径",i,j);
+                            }
+                        }
+                        else
+                        {
+                            Console.Write("从{0}到{1}的路径为：", i, j);
+                            Console.Write(i + "→");
+                            GetPath2(path, i, j);
+                            Console.WriteLine(j);
+                            Console.WriteLine("     路径长度为："+A[i,j]);
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        static void GetPath2(int[,] path,int i,int j)
+        {
+            int k = path[i, j];
+            if(k==-1)
+            {
+                return;
+            }
+            GetPath2(path, i, k);
+            Console.Write(k + "→");
+            GetPath2(path, k, j);
+        }
+        
+    }
+}
